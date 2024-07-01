7f16e18abebb889cd56c4d4809e55be140bd8b10
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-01 09:46:57 +0530

    basic

---
+++ Basic/BasicDemo1.js
@@ -0,0 +1,27 @@
+// console.log("Log function called");
+// console.info("log function called");
+// console.warn("Warn function called")
+// console.error("Error function called");
+
+
+// Data types : number , string , boolean , undefined
+
+a = 10;
+console.log("Value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = 12.54;
+console.log("Value of b = ",parseInt(b));
+console.log("type of b = ", typeof(b));
+
+c = "ram";
+console.log("Value of c = ",c);
+console.log("type of c = ", typeof c);
+
+isActive = true;
+console.log("value of isActive = ",isActive);
+console.log("type of isActive = ",typeof isActive);
+
+var abc;
+console.log("abc = ",abc);
+console.log("type of abc = ",typeof abc);
