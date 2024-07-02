c1c8b4debec8bc00c368e25a2e93f6212f9e6c30
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-02 09:55:03 +0530

    input

---
+++ Basic/BasicDemo2.js
@@ -0,0 +1,41 @@
+// var a = 10;
+// console.log("outer block value of a = ",a);
+
+// let b = 12.4;   // block 
+// console.log("outer block value of b = ",b);
+
+
+
+    // console.log("inner block value of a = ",a);
+    // console.log("inner block value of b = ",b);
+
+//     var a = 10;
+//     console.log("inner block value of a = ",a);
+
+//     let b = 12.4;
+//     console.log("inner block value of b = ",b);
+// }
+
+// console.log("outer block value of a = ",a);
+// console.log("outer block value of b = ",b);
+
+
+// var a = 10;
+// console.log("value of a = ",a);
+
+// var a = 20;
+// console.log("value of a = ",a);
+
+
+// let x = 12.5;
+// console.log("value of x = ",x);
+
+// x = 25.5;
+// console.log("value of x = ",x);
+
+const pi = 3.14;
+console.log("value of pi = ",pi);
+
+
+pi = 3.15;
+console.log("value of pi = ",pi);
---
+++ Input/Input.js
@@ -0,0 +1,20 @@
+// alert("Alert functoion called");
+
+// var x = confirm("Are u single or not");
+// console.log(x);
+
+
+// var x = prompt("Enter your name");
+// console.log(x);
+
+
+var x = parseInt(prompt("Enter 1 number :-"))
+var y = parseInt(prompt("Enter 2 number :-"))
+
+// console.log("Addition = ",x+y);
+
+// var add = x + y;
+// // console.log("Addition = ",(x +y));
+// console.log("addition = ",add);
+
+alert("Addition = " + (x + y))
