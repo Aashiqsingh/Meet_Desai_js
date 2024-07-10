1eda93bd0391be02bde69b2f0ef32a413c3333ed
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-10 09:41:06 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,27 @@
+// Array -- Array is a collection of data 
+// Array - heterogeneous
+
+
+// var arry = ["ufkuywr",65764,true]
+
+var users = ["meet","sagar","rahul","tejas","bhuwan","pukar","roshan"]
+
+// console.log(users);
+// console.log(users[1]);
+
+// for(let i=0;i<users.length;i++) {
+//     console.log(users[i]);
+// }
+
+// Add a new user
+
+var x = users.push("Dhiraj")
+console.log(x);
+
+console.log(users);
+
+
+users.pop();
+console.log(users);
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,50 @@
+var users = ["meet","sagar","rahul ","tejas","bhuwan","pukar","roshan"]
+
+
+
+var arr = [1,2,3,4,5,6,7,8,9,10,11]
+// users.unshift("Dhiraj")
+
+// console.log(users);
+
+
+// var x = users.shift()
+// console.log(x);
+// console.log(users);
+
+// var x = users.concat(arr)
+// console.log(x);
+
+// var x = users.includes("rahul ")
+// console.log(x);
+
+// var x = users.indexOf("aashiq")
+// console.log(x);
+
+// var x = users.reverse()
+// console.log(x);
+
+// var x = users.slice(1)
+// console.log(x);
+
+// var x = users.slice(1,4)
+// console.log(x);
+
+
+// var x = users.splice(1,2)
+// console.log(x);
+
+// console.log(users);
+
+// var x = users.splice(1,2,"ram","krishna")
+// console.log(x);
+
+// console.log(users);
+
+// var x = users.sort()
+// console.log(x);
+
+var arry = [23,45,22,12,13,14,55,23,12,45,11]
+
+var x = arry.sort()
+console.log(x);
