bd671dbf3899d2ab9afed52446b7f321d5c4875a
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-30 09:59:27 +0530

    Date

---
+++ Date/Date.js
@@ -0,0 +1,34 @@
+// Date is class in javascript 
+
+function FetchDate(){
+
+    // var date = new Date();
+    // console.log(date);
+    // console.log(date.getFullYear());
+    // console.log(date.getMonth()+1);
+    // console.log(date.getDate());
+    // console.log(date.getHours());
+    // console.log(date.getMinutes());
+    // console.log(date.getSeconds());
+    // console.log(date.getDay());
+
+
+
+    var date = new Date()
+    date.setDate(14)
+    date.setMonth(1)
+    date.setFullYear(2019)
+    date.setHours(10)
+    date.setMinutes(30)
+    date.setSeconds(0)
+    
+
+    console.log(date);
+
+
+
+}
+
+
+
+FetchDate()
